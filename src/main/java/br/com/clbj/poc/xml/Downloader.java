f30339b4108233a37b9309ac15bf4475af30b9c4
clbj <clbj@optimus.home>
2014-04-21 23:21:24 -0300

    Ajustes no método de download de url

--- src/main/java/br/com/clbj/poc/xml/Downloader.java
+++ src/main/java/br/com/clbj/poc/xml/Downloader.java
@@ -6,6 +6,10 @@
 
 package br.com.clbj.poc.xml;
 
+import br.com.clbj.util.FileHandler;
+import java.io.IOException;
+import java.util.HashMap;
+
 /**
  *
  * @author clbj
@@ -15,8 +19,21 @@ public class Downloader {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        // ceffd92e39d802caf18d5f514a20a178
+        System.out.println("Iniciando execução ...");
+        for (int i = 200; i < 210; i++) {
+            HashMap<String,Object> params = new HashMap<>();
+            HashMap<String,String> headers = new HashMap<>();
+            params.put("url", "http://jaws.clicrbs.com.br/gaucha/site/news-article/published/"+i);
+            params.put("path", "/tmp/gaucha");
+            params.put("overwrite", Boolean.TRUE);
+            params.put("extension", "xml");
+            headers.put("JAWS-KEY", "ceffd92e39d802caf18d5f514a20a178");
+            FileHandler.urlToFile(params, headers);
+        }
+        System.out.println("Finalizado.");
     }
     
     
--- src/main/java/br/com/clbj/util/FileHandler.java
+++ src/main/java/br/com/clbj/util/FileHandler.java
@@ -3,7 +3,6 @@
  *
  * Copyright © CLBJ - Cloves Langendorf Barcellos Junior
  */
-
 package br.com.clbj.util;
 
 import java.io.File;
@@ -12,41 +11,60 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.StringTokenizer;
 import com.google.common.io.ByteStreams;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
+ * Classe para manipulação de arquivos.
  *
- * @author clbj
+ * @author Cloves Langendorf Barcellos Junior
+ * @version ${project.version}
  */
 public class FileHandler {
 
     /**
      * Armazena todo o conteúdo de uma url em um arquivo.
-     * 
-     * @param url
-     * @param outputPath
-     * @param overwrite
+     *
+     * @param params
+     * @param headers
      * @return
-     * @throws IOException 
+     * @throws IOException
      */
-    public static String urlToFile(String url, String outputPath, Boolean overwrite) throws IOException {
-
-        String filename = url;
-        StringTokenizer tokens = new StringTokenizer(url, "/");
+    public static String urlToFile(HashMap<String, Object> params, HashMap<String, String> headers) throws IOException {
+        String fileExtension = "";
+        String outputPath = params.get("path").toString();
+        String filename = params.get("url").toString();
+        StringTokenizer tokens = new StringTokenizer(params.get("url").toString(), "/");
         while (tokens.hasMoreElements()) {
             filename = tokens.nextElement().toString();
         }
-        File file = new File(outputPath.concat(filename));
-        
-        if (overwrite || !file.exists()) {
-            URL inputUrl = new URL(url);
 
+        if (!outputPath.endsWith("/")) {
+            outputPath += "/";
+        }
+        
+        if (params.containsKey("extension")) {
+            fileExtension = params.get("extension").toString().toLowerCase();
+            if (!fileExtension.startsWith(".")) {
+                fileExtension = "." + fileExtension;
+            }
+        }
+
+        File file = new File(outputPath.concat(filename).concat(fileExtension));
+
+        if ((Boolean) params.get("overwrite") || !file.exists()) {
+            URLConnection connection = new URL(params.get("url").toString()).openConnection();
+            if (!headers.isEmpty()) {
+                for (Map.Entry<String, String> entry : headers.entrySet()) {
+                    connection.setRequestProperty(entry.getKey(), entry.getValue());
+                }
+            }
             file.getParentFile().mkdirs();
             file.createNewFile();
             file.setWritable(true);
-
-            ByteStreams.copy(inputUrl.openStream(), new FileOutputStream(file));
+            ByteStreams.copy(connection.getInputStream(), new FileOutputStream(file));
         }
-
         return file.getAbsolutePath();
     }
 }
